c5eaa8dab69bfb0a86326a7c6b4b21f2e0b82097
anvaka <anvaka@gmail.com>
2014-02-18 22:45:53 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var directive = require('./lib/directive');
+var controller = require('./lib/controller');
+var filter = require('./lib/filter');
+
+var ngModule;
+
+module.exports = {
+  directive: directive.register,
+  controller: controller.register,
+  filter: filter.register,
+
+  flush: function () {
+    debugger;
+    // todo: accept module;
+    if (!ngModule) {
+      ngModule = createModule();
+    }
+
+    controller.flush(ngModule);
+    directive.flush(ngModule);
+    filter.flush(ngModule);
+
+    angular.bootstrap(document, [ngModule.name]);
+
+    return ngModule;
+  }
+};
+
+function createModule() {
+  return angular.module('anModule', []);
+}
